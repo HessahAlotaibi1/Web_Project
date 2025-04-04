d0946d6118423378faa15e4e7fe95f6ccc86decc
v <halooh2004@gmail.com>
2025-04-05 02:13:42 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -188,6 +188,7 @@ function getSpecialities() {
 
 <head>
     <title>Sign-up</title>
+    <link rel="icon" type="image/png" href="images/logo2.png">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/singup.css">
