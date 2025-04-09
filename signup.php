c5029a4c3fdb67c747114d0413812e7580bba6fa
v <halooh2004@gmail.com>
2025-04-09 04:39:33 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -192,7 +192,6 @@ function getSpecialities() {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/singup.css">
-
     <script src="script.js"></script>
 
 </head>
